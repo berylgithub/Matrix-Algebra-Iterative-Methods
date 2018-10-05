b4c66398ddb4d43c69666596a9052cb96b871cbe
DESKTOP-QTCAKUD\YorozuyaSaint <berylramadhian@gmail.com>
2018-10-05 19:40:40 +0700

    resolving push conflict

--- jacobi-gauss_seidel.py
+++ jacobi-gauss_seidel.py
@@ -36,9 +36,44 @@ def linear_iterative_methods(A, b, x, eps, max_iter, method="gauss_seidel", log=
         
 
 if __name__ == "__main__":
+    #example at page 451 Burden-Faires book
     x=np.zeros((4))
     A=np.array([[10,-1,2,0],[-1,11,-1,3],[2,-1,10,-1],[0,3,-1,8]])
     b=np.array([6, 25, -11, 15])
-    x, k=linear_iterative_methods(A, b, x, 0.001, 20)
-
+    x, k=linear_iterative_methods(A, b, x, 0.001, 20, method = 'gauss_seidel')
     
+    #exercise : 9.b
+    x=np.zeros((3))
+    A=np.array([[2,-1,1], [2, 2, 2], [-1, -1, 2]])
+    b=np.array([-1, 4, 5])
+    x, k=linear_iterative_methods(A, b, x, 0.001, 100, method='jacobian')
+    
+    #exercise : 9.d
+    x=np.zeros((3))
+    A=np.array([[2,-1,1], [2, 2, 2], [-1, -1, 2]])
+    b=np.array([-1, 4, 5])
+    x, k=linear_iterative_methods(A, b, x, 0.00001, 100)
+    
+    #exercise : 10.b
+    x=np.zeros((3))
+    A=np.array([[1, 2, -2], [1, 1, 1], [2, 2, 1]])
+    b=np.array([7, 2, 5])
+    x, k=linear_iterative_methods(A, b, x, 0.00001, 100, method='jacobian')
+    
+    #exercise : 10.d
+    x=np.zeros((3))
+    A=np.array([[1, 2, -2], [1, 1, 1], [2, 2, 1]])
+    b=np.array([7, 2, 5])
+    x, k=linear_iterative_methods(A, b, x, 0.00001, 100)
+    
+    #exercise : 11.c
+    x=np.zeros((3))
+    A=np.array([[1, 0, -1], [-1/2, 1, -1/4], [1, -1/2, 1]])
+    b=np.array([0.2, -1.425, 2])
+    x, k=linear_iterative_methods(A, b, x, 0.01, 300)
+
+    #exercise : 11.d
+    x=np.zeros((3))
+    A=np.array([[1, 0, -2], [-1/2, 1, -1/4], [1, -1/2, 1]])
+    b=np.array([0.2, -1.425, 2])
+    x, k=linear_iterative_methods(A, b, x, 0.01, 300)
